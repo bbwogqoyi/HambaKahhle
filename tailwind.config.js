ebe37dffe9428c9f98604675cc6e2958a576d4d7
Bonani Brian Wogqoyi <bbwogqoyi@outlook.com>
2020-09-14 00:17:15 +0200

    initial work on the admin dashboard

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,6 +3,14 @@ module.exports = {
   purge: [],
   theme: {
     extend: {
+      colors: {
+        'myrtle-green': '#2B7A78',
+        'Azure-x-11': '#DEF2F1',
+        'navy-blue': '#00007A',
+        'snow-white': '#FFFBFC',
+        'intl-orange-eng': '#BC2C1A',
+        'baby-powder': '#FFFFFA'
+      }
     },
   },
   variants: {
