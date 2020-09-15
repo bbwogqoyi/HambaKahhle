5d3cd15889a5d0bc3cbb7440c17525d0120bf23b
Bonani Brian Wogqoyi <bbwogqoyi@outlook.com>
2020-09-15 17:42:17 +0200

    admin dashboard topbar-nav and initial work on the content

--- tailwind.config.js
+++ tailwind.config.js
@@ -6,10 +6,11 @@ module.exports = {
       colors: {
         'myrtle-green': '#2B7A78',
         'Azure-x-11': '#DEF2F1',
-        'navy-blue': '#00007A',
+        'navy-blue': '#0112FC',
         'snow-white': '#FFFBFC',
         'intl-orange-eng': '#BC2C1A',
-        'baby-powder': '#FFFFFA'
+        'baby-powder': '#FFFFFA',
+        'light-grayish-blue': '#f2f4fa'
       }
     },
   },
