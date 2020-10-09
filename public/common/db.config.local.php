77e1235593b9c0cbc0bfd9d75ec4a94fd11c207a
Bonani Brian Wogqoyi <bbwogqoyi@outlook.com>
2020-10-09 22:01:33 +0200

    feat: company assets dashboard page with functionality for add new records, review, modify and delete existing entries

--- public/common/db.config.local.php
+++ public/common/db.config.local.php
@@ -13,11 +13,3 @@
   define("PASSWORD", "SysD3V2020");
   define("DATABASE", "blackpeakltd");
 ?>
-
-<?php
-  // database credentials
-  define("SERVER", "127.0.0.1:3308");
-  define("USERNAME", "BlackPeakLtd");
-  define("PASSWORD", "SysD3V2020");
-  define("DATABASE", "blackpeakltd");
-?>
