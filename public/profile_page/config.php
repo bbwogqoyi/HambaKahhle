ce9fb48f3116f605ffe68f6e93a389255ab5904c
Lesley-nashe <31068181+Lesley-nashe@users.noreply.github.com>
2020-10-18 17:10:51 +0200

    Add files via upload
    profile page

---
+++ public/profile_page/config.php
@@ -0,0 +1,7 @@
+<?php
+  // database credentials
+  define("SERVERNAME", "is3-dev.ict.ru.ac.za");
+  define("USERNAME", "BlackPeakLtd");
+  define("PASSWORD", "SysD3V2020");
+  define("DATABASE", "blackpeakltd");
+?>
