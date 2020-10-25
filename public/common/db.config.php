e321d5b1859ffbe34dc0eba654da2a5b547b00cc
Bonani Brian Wogqoyi <bbwogqoyi@outlook.com>
2020-10-26 00:26:03 +0200

    feat(validation): styles for the input

--- public/common/db.config.php
+++ public/common/db.config.php
@@ -1,6 +1,6 @@
 <?php
   // database credentials
-  define("SERVER", "is3-dev.ict.ru.ac.za");
+  define("SERVER", "127.0.0.1:3308");
   define("USERNAME", "BlackPeakLtd");
   define("PASSWORD", "SysD3V2020");
   define("DATABASE", "blackpeakltd");
